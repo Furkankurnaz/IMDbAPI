00ef97391d63d82fbfb72eb9062e8cf88cf31f0d
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-16 09:31:24 +0300

    adds a function to SearchViewModel to request

--- iMDbAPI/Classes/ViewModels/SearchViewModel.swift
+++ iMDbAPI/Classes/ViewModels/SearchViewModel.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Moya
 
 class SearchViewModel {
     private var types: [String] = []
     private var years: [String] = []
+    
+    private var searchResults: SearchModel!
     
     func getTypes() -> [String] {
         types = [
@@ -30,6 +33,30 @@ class SearchViewModel {
         }
         
         return years
+    }
+    
+    func getMedia(title: String, type: String?, year: String) {
+        let pluginsArray:[PluginType] = [NetworkLoggerPlugin(cURL: true)]
+        let provider = MoyaProvider<IMDbAPIService>(plugins: pluginsArray)
+        
+        provider.request(.search(title: title, type: type, year: year)) { (response) in
+            switch response {
+            case .success(let value):
+                let data = value.data
+                
+                do {
+                    let results = try JSONDecoder().decode(SearchModel.self, from: data)
+                    
+                    self.searchResults = results
+                    
+                } catch let error {
+                    print(error)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
 }
